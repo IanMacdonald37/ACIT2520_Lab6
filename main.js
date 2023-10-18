a714144a3cc64f93e8833d68afe9b68f0e39ba92
Ian Macdonald <imacdonald37@my.bcit.ca>
2023-10-18 11:18:04 -0700

    main.js logic itt 1

--- main.js
+++ main.js
@@ -10,12 +10,16 @@ const path = require("path");
  */
 
 const IOhandler = require("./IOhandler");
+const { readdir } = require("fs");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
 
-// step 1 read the zip
-// step 2 unzip the file
-// step 3 read all png images
-// step 4 send them to gray scale
-// step 5 after all r unzipped print a success message
+
+zipProm = IOhandler.unzip(zipFilePath, pathUnzipped)
+zipProm.then(IOhandler.readdir(pathUnzipped).then( value = {
+    Promise.all([IOhandler.grayScale(value[0]), IOhandler.grayScale(value[1]), IOhandler.grayScale(value[2])]).then(
+        console.log("gray filtering complete")
+    )
+})
+)
